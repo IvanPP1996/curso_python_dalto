25b238b3835661b5ca86d7941a6e98eef3ec2273
Ivan Patiño <ivanr.p.p.2020@gmail.com>
2024-11-07 10:51:39 -0500

    Primer modificacion

---
+++ 1 - Curso Python/holaMundo.py
@@ -0,0 +1,2 @@
+print("Hola mundo")
+print("Hola mundo, Gracias Dalto por enseñarme Python")
---
+++ 1 - Curso Python/tiposDeDatos/tiposDeDatos.py
@@ -0,0 +1,21 @@
+#Texto o string
+"string"
+'string'
+#Permite hacer saltos de linea o escribir texto aparte
+"""
+string
+nombre = Javier
+apellido = Figueroa
+"""
+#Números enteros
+5
+565762
+322
+-433
+#Números flotantes
+40.3
+2334.89
+-8788.98
+#Booleano (se escriben en Amyuscula la primera letra)
+True
+False
